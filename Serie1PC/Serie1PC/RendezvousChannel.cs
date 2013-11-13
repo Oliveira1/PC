b6bf39e2ad13b893175b5c124c1caeb8defc13ec
Oliveira1 <weare@live.com.pt>
2013-11-13 23:03:19 +0000

    Adicionado o Rendezvous Testado ok.!

--- Serie1PC/Serie1PC/RendezvousChannel.cs
+++ Serie1PC/Serie1PC/RendezvousChannel.cs
@@ -1,27 +1,132 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading;
+
 
 namespace Serie1PC
 {
-    class RendezvousChannel<S,R>
+    public class RendezvousChannel<S,R>
     {
+        public class Token
+        {
+            public S service;
+            public Status processing=Status.Open;
+            public R response;
+        }
+        private LinkedList<Token> _services;
+        private LinkedList<Token> _processingRequests;
+        public enum Status{Open,Accepting, InProcess, Concluded }
+        public RendezvousChannel()
+        {
+            _services=new LinkedList<Token>();
+            _processingRequests = new LinkedList<Token>();
+        }
         public bool Request(S service,int timeout, out R response)
         {
-            throw new NotImplementedException();     
+            lock (this)
+            {
+                Token myToken;
+                if(CheckForAcceptingTokens(out myToken,service))
+                {
+                   Monitor.PulseAll(this);
+                }
+                else
+                {
+                     myToken.service = service;
+                     _services.AddLast(myToken);
+                }
+                    
+                while (true)
+                {
+                    Monitor.Wait(this);
+                    if (myToken.processing == Status.Concluded)
+                    {
+                        response = myToken.response;
+                        return true;
+                    }
+                }
+            }
+        }
+        private bool CheckForAcceptingTokens(out Token myToken, S service)
+        {
+            foreach (var token in _services)
+            {
+                if (token.processing == Status.Accepting)
+                {
+                    myToken = token;
+                    myToken.service = service;
+                    myToken.processing=Status.InProcess;
+                    _services.Remove(token);
+                    _processingRequests.AddLast(token);
+                    return true;
+                }
+            }
+            myToken=new Token();
+            return false;
         }
 
-    public  object Accept(int timeout, out S service)
+        public  object Accept(int timeout, out S service)
     {
-        throw  new NotImplementedException();
-    }
-
-
-       public void Reply(object rendezVousToken, R Response)
+        lock (this)
         {
-            throw new NotImplementedException();    
+            var myToken=new Token();
+            if (_services.Count !=0)
+            {
+                RemoveFirstRequestedToken(ref myToken);
+                Monitor.PulseAll(this);   // modificar, para já é sem notificação específica de Threads
+                service = myToken.service;
+                return myToken;
+            }
+            myToken.processing = Status.Accepting;
+            _services.AddLast(myToken);
+            while (true)
+            {
+                Monitor.Wait(this);
+                if (myToken.processing==Status.InProcess)
+                {
+                    service = myToken.service;
+                    return myToken;
+                }
+            }
+        }
+
+    }
+    public void Reply(object rendezVousToken, R Response)
+        {
+            Token myToken;
+            lock (this)
+            {
+                myToken = (Token) rendezVousToken;
+                FindTokenInProcessingRequests(ref myToken,Response);
+                Monitor.PulseAll(this);
+            }
+        }
+        private void RemoveFirstRequestedToken(ref Token myToken)
+        {
+            foreach (var token in _services)
+            {
+                if (token.processing == Status.Open)
+                {
+                    myToken = token;
+                    myToken.processing = Status.InProcess;
+                    _services.Remove(token);
+                    _processingRequests.AddLast(token);
+                    return;
+                }
+
+            }
+            
+        }
+        private void FindTokenInProcessingRequests(ref Token myToken, R response)
+        {
+
+                var linkedListNode = _processingRequests.Find(myToken);
+                if (linkedListNode != null)
+                {
+                    Token request = linkedListNode.Value;
+                    request.processing = Status.Concluded;
+                    request.response = response;
+                    _processingRequests.Remove(request);
+                }
         }
     }
 }
---
+++ Serie1PC/TestsSerie1Pc/TestRendezvousChannel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Serie1PC;
+
+namespace TestsSerie1Pc
+{
+    [TestClass]
+    public class TestRendezvousChannel
+    {
+
+        [TestMethod]
+        public void WithOneClientAndOneServerThread()
+        {   
+            var synchronizer = new RendezvousChannel<int,int>();
+            var waitingToStart = new ManualResetEvent(false);
+            var server=new Thread(() =>
+            {
+                int service;
+                waitingToStart.Set();
+                var myToken=(RendezvousChannel<int,int>.Token)synchronizer.Accept(0,out service);
+                synchronizer.Reply(myToken,myToken.service*2);
+            });
+         
+            server.Start();
+                int response;
+            waitingToStart.WaitOne();
+            if (synchronizer.Request(2, 0, out response))
+            {
+                Assert.AreEqual(4,response);
+            }
+        }
+    }
+}
