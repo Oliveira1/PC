1030b81fceac4c072225e5a04b549ff716a52371
Oliveira1 <weare@live.com.pt>
2013-12-16 19:40:01 +0000

    final commit

---
+++ NonBlockingCompletion/NonBlockingCompletion/NonBlockingCompletion.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NonBlockingCompletion
+{
+    class NonBlockingCompletion
+    {
+        static void Main(string[] args)
+        {
+        }
+
+
+
+        private  volatile int permits = 0;
+
+        public void WaitForCompletion()
+        {
+            
+            do
+            {
+                var curPermits = permits;
+			if(curPermits==-1) return;
+			if(curPermits>0)
+				if(Interlocked.CompareExchange(ref permits,curPermits,permits-1)==curPermits)
+                    if(permits==(curPermits-1))
+					    return;
+
+			if(permits>0) continue;
+                lock (this)
+                {
+
+                    Monitor.Wait(this);
+
+                }
+
+		}while(true);
+        }
+
+
+        public void complete(){
+		do{
+			int curPermits=permits;
+			if(curPermits==-1) return;
+		    if (Interlocked.CompareExchange(ref permits, curPermits, permits - 1) == curPermits)
+		    {
+		        if (permits == (curPermits + 1))
+		            lock (this)
+		            {
+		                Monitor.Pulse(this); 
+                        return;
+		            }
+		    }
+			
+		}while(true);
+
+
+	}
+        public void completeAll(){
+		int i=0;
+		while(permits!=-1){
+			permits=-1;
+		}
+		lock(this){
+			Monitor.PulseAll(this);
+		}
+	}
+
+
+    }
+}
---
+++ NonBlockingCompletion/NonBlockingCompletion/NonBlockingConcurrentQueue.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NonBlockingCompletion
+{
+   public class NonBlockingConcurrentQueue<T>
+    {
+
+      private sealed class Node
+       {
+           public Node _next;
+           public T _elem;
+           public Node(T elem, Node next)
+           {
+                _elem = elem;
+                _next = next;
+           }
+        }
+
+
+	    private readonly Node _dummy = new Node(default(T), null);
+       private Node _head;
+       private  Node _tail;
+
+	public NonBlockingConcurrentQueue()
+	{
+	    _head = _dummy;
+	    _tail = _dummy;
+	}
+
+	public void put (T item){
+		Node newNode = new Node(item, null); 
+		while (true)
+		{
+		    Node curTail = _tail;
+		    Node tailNext = curTail._next;
+			if (curTail == _tail) { 
+				if (tailNext != null) { 
+					// Queue in intermediate state, advance tail 
+				    Interlocked.CompareExchange(ref _tail, tailNext, curTail);
+				} else { 
+					// In quiescent state, try inserting new node
+				    if (Interlocked.CompareExchange(ref curTail._next, newNode, null) == null)
+				    {
+				        // Insertion succeeded, try advancing tail 
+				        Interlocked.CompareExchange(ref _tail, newNode, curTail);
+				        return;
+				    }
+				} 
+				} 
+			} 
+		} 
+
+
+	
+	public T tryTake(){
+
+		do
+		{
+		    Node curHead = _head;
+		    Node headNext = curHead._next;
+		    Node curTail = _tail;
+
+			if(headNext==null)return default(T);
+
+			//estado intermedio
+			if(curHead==curTail)	
+		     Interlocked.CompareExchange(ref _tail, headNext, curTail);
+			
+			//estado normal
+			else if(Interlocked.CompareExchange(ref _head,curHead,headNext)==null)
+				return headNext._elem;
+
+
+
+		}while(true);
+
+	}
+
+	public Boolean isEmpty()
+	{
+	    return (_head._next == null);
+	}
+
+
+
+    }
+}
